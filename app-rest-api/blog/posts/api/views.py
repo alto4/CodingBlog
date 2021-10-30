398dacf2769f180e78fb2b50948856b3c8d0761d
Scott Alton <scottaltonmusic@gmail.com>
2021-10-30 14:47:32 -0400

    Initial Django and React setup with MUI.

---
+++ app-rest-api/blog/posts/api/views.py
@@ -0,0 +1,15 @@
+from django.db.models import query
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from posts.models import Post
+from .serializers import PostSerializer
+
+
+class PostListView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
