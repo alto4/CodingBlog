2286f472e979c9bdedd9a581e46282b49452ddb4
Scott Alton <scottaltonmusic@gmail.com>
2021-10-30 18:09:02 -0400

    Refactored api to use viewsets to handle CRUD for posts.

--- app-rest-api/blog/posts/api/urls.py
+++ app-rest-api/blog/posts/api/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
+from django.db import router
+from rest_framework import urlpatterns
+from posts.api.views import PostViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+router = DefaultRouter()
+router.register('', PostViewSet, basename='posts')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('', PostListView.as_view()),
-    path('create/', PostCreateView.as_view()),
-    path('<pk>', PostDetailView.as_view()),
-    path('<pk>/update/', PostUpdateView.as_view()),
-    path('<pk>/delete/', PostDeleteView.as_view()),
-]
+# from django.urls import path
+
+# from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
+# urlpatterns = [
+#     path('', PostListView.as_view()),
+#     path('create/', PostCreateView.as_view()),
+#     path('<pk>', PostDetailView.as_view()),
+#     path('<pk>/update/', PostUpdateView.as_view()),
+#     path('<pk>/delete/', PostDeleteView.as_view()),
+# ]
--- app-rest-api/blog/posts/api/views.py
+++ app-rest-api/blog/posts/api/views.py
@@ -1,30 +1,37 @@
 from django.db.models import query
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework import viewsets
 
 from posts.models import Post
 from .serializers import PostSerializer
 
 
-class PostListView(ListAPIView):
-    queryset = Post.objects.all()
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
-
-
-class PostDetailView(RetrieveAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+# PRE-REFACTORING: Django Rest Framework viewsets can be used to encapsulate all CRUD operations baked into one queryset
+# from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+
+# class PostListView(ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 
-class PostCreateView(CreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostDetailView(RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 
-class PostUpdateView(UpdateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostCreateView(CreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 
-class PostDeleteView(DestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostUpdateView(UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class PostDeleteView(DestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
