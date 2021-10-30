754bf49944c3d66a2059289dac352c47fe60f2a7
Scott Alton <scottaltonmusic@gmail.com>
2021-10-30 17:23:08 -0400

    Added post update and delete views to API.

--- app-rest-api/blog/posts/api/urls.py
+++ app-rest-api/blog/posts/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view()),
-    path('<pk>', PostDetailView.as_view())
+    path('create/', PostCreateView.as_view()),
+    path('<pk>', PostDetailView.as_view()),
+    path('<pk>/update/', PostUpdateView.as_view()),
+    path('<pk>/delete/', PostDeleteView.as_view()),
 ]
--- app-rest-api/blog/posts/api/views.py
+++ app-rest-api/blog/posts/api/views.py
@@ -1,5 +1,5 @@
 from django.db.models import query
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
 from posts.models import Post
 from .serializers import PostSerializer
@@ -11,5 +11,20 @@ class PostListView(ListAPIView):
 
 
 class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostCreateView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostUpdateView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDeleteView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
